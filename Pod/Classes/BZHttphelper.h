2e462d5ee8b022f20625030f0c5a7feb5a9d06e9
xiangfp <457813584@qq.com>
2016-01-07 16:19:10 +0800

    first commit

---
+++ Pod/Classes/BZHttphelper.h
@@ -0,0 +1,13 @@
+//
+//  BZHttphelper.h
+//  Pods
+//
+//  Created by xiangfp on 16/1/7.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BZHttphelper : NSObject
+
+@end
---
+++ Pod/Classes/BZHttphelper.m
@@ -0,0 +1,13 @@
+//
+//  BZHttphelper.m
+//  Pods
+//
+//  Created by xiangfp on 16/1/7.
+//
+//
+
+#import "BZHttphelper.h"
+
+@implementation BZHttphelper
+
+@end
